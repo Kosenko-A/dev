fb25daf286b5c79f385c665f04639d90dc9e8a24
Kosenko <Kosenko.2000@yandex.ru>
2022-12-04 20:57:58 +0500

    add project

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+
+public class AppTest 
+
+{
+    @Test
+    void testCalcOne()
+    {
+        int res = 4;
+        Assertions.assertEquals(4,res);
+
+    }
+}
